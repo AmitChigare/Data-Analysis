0b8aca9395a97a71ce06b4bd7567897174f52bcf
Amituttam C <amituttam.c@cspl.com>
2023-06-13 12:03:24 +0530

    new branch

--- excel_manager/urls.py
+++ excel_manager/urls.py
@@ -5,12 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("stacked_bar_method/", views.stacked_bar_method, name="stacked_bar_method"),
-    path(
-        "files/<str:month1>/<str:month2>/<str:field>/<str:field2>/",
-        views.display_files,
-        name="display_files",
-    ),
     path(
         "lines/<str:month1>/<str:month2>/<str:field2>/<str:field1>/",
         views.line_graph,
--- excel_manager/views.py
+++ excel_manager/views.py
@@ -1,37 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ExcelFile
 import re
 from django.db.models import Q
-from django.urls import reverse
 from datetime import datetime
 
 import calendar
 
 import pandas as pd
-import plotly.express as px
 import pandas as pd
-import plotly.subplots as sp
 import plotly.graph_objects as go
 import plotly.io as pio
 
 months_years = [
     f"{month}-{year}" for year in range(2022, 2024) for month in calendar.month_name[1:]
-]
-
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
 ]
 
 # Mapping of keywords to months
@@ -107,205 +89,47 @@ def home(request):
 
     return render(request, "index.html")
 
+field1='Resource Group Name'
+excel_files = ExcelFile.objects.all().order_by("month")
 
-def stacked_bar_method(request):
-    if request.method == "POST":
-        month1 = request.POST.get("month1")
-        month2 = request.POST.get("month2")
-        field = request.POST.get("field")
-        field2 = request.POST.get("field2")
-        # method = request.POST.get("method")
+# if not excel_files:
+#     # No data within the specified month range
+#     message = f"<h1>No files in the database. Please upload</h1>"
+#     return HttpResponse(message)
 
-        return redirect(
-            "display_files",
-            month1=month1,
-            month2=month2,
-            field=field,
-            field2=field2,
-            # method=method,
+# Create a dictionary to store the combined data for each month
+combined_data_dict = {}
+for excel_file in excel_files:
+    excel_file_path = excel_file.file.path
+
+    # Read Excel file into a DataFrame using pandas, skipping the first 10 rows and last row
+    df = pd.read_excel(excel_file_path, skiprows=10, skipfooter=1, usecols=[field1])
+
+    # Keep only the desired columns: field1, field2, and 'Amount'
+    # df = df[[field1]]
+
+    # Get the month of the current file
+    file_month = excel_file.month.strftime("%B")
+
+    if file_month not in combined_data_dict:
+        # If the month is not already in the dictionary, create a new key-value pair
+        combined_data_dict[file_month] = df
+    else:
+        # If the month is already in the dictionary, concatenate the DataFrame with the existing data
+        combined_data_dict[file_month] = pd.concat(
+            [combined_data_dict[file_month], df], ignore_index=True
         )
 
-    return render(request, "bar.html", {"months": months})
+# Combine unique values of field1 for every month
+combined_field1_values = []
+for df in combined_data_dict.values():
+    unique_values = df[field1].unique().tolist()
+    combined_field1_values.extend(unique_values)
 
-
-def display_files(
-    request,
-    month1="January",
-    month2="February",
-    field="Location",
-    field2="Meter Name",
-    # method="Month",
-):
-    # Convert month names to datetime objects
-    datetime_month1 = datetime.strptime(month1, "%B")
-    datetime_month2 = datetime.strptime(month2, "%B")
-
-    query = Q(
-        month__month__gt=datetime_month1.month, month__month__lte=datetime_month2.month
-    )
-
-    excel_files = ExcelFile.objects.filter(query)
-    if not excel_files:
-        # No data within the specified month range
-        message = f"<h1>No data available from {month1} to {month2}</h1>"
-        return HttpResponse(message)
-
-    # Create a dictionary to store the combined data for each month
-    combined_data_dict = {}
-
-    for excel_file in excel_files:
-        excel_file_path = excel_file.file.path
-
-        # Read Excel file into a DataFrame using pandas, skipping the first 10 rows and last row
-        df = pd.read_excel(excel_file_path, skiprows=10, skipfooter=1)
-
-        # Get the month of the current file
-        file_month = excel_file.month.strftime("%B")
-
-        # Get the project_name
-        project_name = excel_file.project_name
-
-        # if method == "Month":
-        if file_month not in combined_data_dict:
-            # If the month is not already in the dictionary, create a new key-value pair
-            combined_data_dict[file_month] = df
-        else:
-            # If the month is already in the dictionary, concatenate the DataFrame with the existing data
-            combined_data_dict[file_month] = pd.concat(
-                [combined_data_dict[file_month], df], ignore_index=True
-            )
-        # else:
-        #     if project_name not in combined_data_dict:
-        #         # If the month is not already in the dictionary, create a new key-value pair
-        #         combined_data_dict[project_name] = df
-        #     else:
-        #         # If the month is already in the dictionary, concatenate the DataFrame with the existing data
-        #         combined_data_dict[project_name] = pd.concat(
-        #             [combined_data_dict[project_name], df], ignore_index=True
-        #         )
-
-    # Create the Plotly subplots for the combined data
-    fig = sp.make_subplots(
-        rows=1,
-        cols=len(combined_data_dict),
-        subplot_titles=list(combined_data_dict.keys()),
-    )
-
-    col_num = 1
-
-    for month, combined_data in combined_data_dict.items():
-        combined_data_dict[month] = df[["Amount", field, field2]]
-
-        # Group by field1 and aggregate the amounts
-        df_grouped_field1 = df.groupby(field)["Amount"].sum().reset_index()
-
-        # Update the DataFrame with the aggregated amounts
-        combined_data_dict[month] = combined_data_dict[month].merge(
-            df_grouped_field1, on=field, suffixes=("", "_sum")
-        )
-
-        # Drop duplicate rows and unnecessary columns
-        combined_data_dict[month] = combined_data_dict[month].drop_duplicates(
-            subset=field
-        )
-        combined_data_dict[month] = combined_data_dict[month][
-            ["Amount_sum", field, field2]
-        ]
-
-        # Group by field2 and aggregate the amounts
-        df_grouped_field2 = df.groupby(field2)["Amount"].sum().reset_index()
-
-        # Update the DataFrame with the aggregated amounts
-        combined_data_dict[month] = combined_data_dict[month].merge(
-            df_grouped_field2, on=field2, suffixes=("", "_sum")
-        )
-
-        # Drop duplicate rows and unnecessary columns
-        combined_data_dict[month] = combined_data_dict[month].drop_duplicates(
-            subset=field2
-        )
-        combined_data_dict[month] = combined_data_dict[month][
-            ["Amount_sum", field, field2]
-        ]
-
-        if not combined_data.empty:
-            # Group the combined data by 'field' and 'field2' and calculate the sum of 'Amount'
-            grouped_data = (
-                combined_data.groupby([field, field2])["Amount"].sum().reset_index()
-            )
-
-            # Sort the data by 'Amount' in descending order for each 'field'
-            sorted_data = (
-                grouped_data.groupby(field)
-                .apply(lambda x: x.sort_values("Amount", ascending=False))
-                .reset_index(drop=True)
-            )
-
-            # Calculate the cumulative sum of 'Amount' for each 'field'
-            sorted_data["Cumulative Amount"] = sorted_data.groupby(field)[
-                "Amount"
-            ].cumsum()
-
-            # Sort the data by 'Amount' in descending order
-            sorted_data = sorted_data.sort_values("Amount", ascending=False)
-
-            sorted_data = sorted_data.head(2)
-
-            # Calculate the total amount for each field
-            total_amount = sorted_data["Amount"].sum()
-
-            # Calculate the percentage taken for each row
-            sorted_data["Percentage Taken"] = (
-                sorted_data["Amount"] / total_amount
-            ) * 100
-
-            # Add the stacked bar traces to the subplot
-            for i, row in sorted_data.iterrows():
-                # Get the color for the current field2 value
-                color = px.colors.qualitative.Alphabet[
-                    i % len(px.colors.qualitative.Alphabet)
-                ]
-
-                fig.add_trace(
-                    go.Bar(
-                        x=[row[field]],
-                        y=[row["Amount"]],
-                        name=row[field2],
-                        marker_color=color,
-                        # text=f"Amount: {row['Amount']:.2f}<br>{row['Percentage Taken']:.2f}%",
-                        text=f"Amount: {row['Amount']:.2f}",
-                        textposition="auto",
-                    ),
-                    row=1,
-                    col=col_num,
-                )
-
-            col_num += 1
-
-    # Update the subplot layout
-    fig.update_layout(
-        title="Stacked Bar Graph by Month",
-        showlegend=False,
-        xaxis=dict(title=field),
-        yaxis=dict(title="Amount"),
-        barmode="stack",
-    )
-
-    # Add a button linking to the /upload URL
-    upload_url = reverse(
-        "home"
-    )  # Assuming the URL name for the upload_file view is "upload_file"
-    upload_button = f'<a href="{upload_url}" style="background-color: #4CAF50;color: white;padding: 10px 20px;border-radius: 5px;cursor: pointer;text-decoration:None;">Go to Home</a>'
-    graph_html = f"{upload_button}<br><br>" + fig.to_html(full_html=False)
-
-    # Create the Django response
-    response = HttpResponse(content_type="text/html")
-
-    # Write the graph HTML to the response
-    response.write(graph_html)
-
-    return response
-
+# Get unique values from the combined list
+combined_field1_values = sorted(
+    [str(value) for value in set(combined_field1_values)]
+)
 
 def line_graph(
     request,
@@ -314,47 +138,6 @@ def line_graph(
     field2="Category",
     field1="Resource Group Name",
 ):
-    excel_files = ExcelFile.objects.all().order_by("month")
-
-    if not excel_files:
-        # No data within the specified month range
-        message = f"<h1>No files in the database. Please upload</h1>"
-        return HttpResponse(message)
-
-    # Create a dictionary to store the combined data for each month
-    combined_data_dict = {}
-    for excel_file in excel_files:
-        excel_file_path = excel_file.file.path
-
-        # Read Excel file into a DataFrame using pandas, skipping the first 10 rows and last row
-        df = pd.read_excel(excel_file_path, skiprows=10, skipfooter=1)
-
-        # Keep only the desired columns: field1, field2, and 'Amount'
-        df = df[[field1, field2, "Amount"]]
-
-        # Get the month of the current file
-        file_month = excel_file.month.strftime("%B")
-
-        if file_month not in combined_data_dict:
-            # If the month is not already in the dictionary, create a new key-value pair
-            combined_data_dict[file_month] = df
-        else:
-            # If the month is already in the dictionary, concatenate the DataFrame with the existing data
-            combined_data_dict[file_month] = pd.concat(
-                [combined_data_dict[file_month], df], ignore_index=True
-            )
-
-    # Combine unique values of field1 for every month
-    combined_field1_values = []
-    for df in combined_data_dict.values():
-        unique_values = df[field1].unique().tolist()
-        combined_field1_values.extend(unique_values)
-
-    # Get unique values from the combined list
-    combined_field1_values = sorted(
-        [str(value) for value in set(combined_field1_values)]
-    )
-
     if request.method == "POST":
         month_year1 = request.POST.get("month_year1")
         month_year2 = request.POST.get("month_year2")
@@ -368,6 +151,7 @@ def line_graph(
         query = Q(month__gte=datetime_month_year1, month__lte=datetime_month_year2)
 
         excel_files = ExcelFile.objects.filter(query).order_by("month")
+        print(1)
 
         if not excel_files:
             # No data within the specified month range
@@ -384,20 +168,18 @@ def line_graph(
             excel_data_dict = pd.read_excel(
                 excel_file_path,
                 sheet_name=None,  # Read all sheets
+                usecols=[field1, field2, 'Amount'],
                 skiprows=10,
                 skipfooter=1,
             )
 
             for sheet_name, df in excel_data_dict.items():
-                # Keep only the desired columns: field1, field2, and 'Amount'
-                df = df[[field1, field2, "Amount"]]
-
                 # Filter the DataFrame based on field1value
                 df = df[df[field1] == field1value]
 
-                # Rename the columns to match the specified field names
-                df.columns = [field1, field2, "Amount"]
-
+                # Groupby field2 
+                df = df.groupby(field2, as_index=False)['Amount'].sum()
+                
                 # Get the month and year of the current sheet
                 sheet_month = excel_file.month.strftime("%B")
                 sheet_year = excel_file.month.strftime("%Y")
@@ -415,8 +197,9 @@ def line_graph(
                         ignore_index=True,
                     )
         # print(combined_data_dict)
+        print(2)
 
-        # Combine unique values of field1 for every month
+        # Combine unique values of field2 for every month
         combined_field2_values = []
         for df in combined_data_dict.values():
             unique_values = df[field2].unique().tolist()
@@ -504,7 +287,7 @@ def line_graph(
             yaxis=dict(title="Amount"),
             title=f'Amount for "{field1value}" in "{field2}" from {month_year1} to {month_year2}',
         )
-
+        print(3)
         # fig.show()
         div = pio.to_html(fig, full_html=False)
 
@@ -514,7 +297,6 @@ def line_graph(
             "graph": div,
         }
         return render(request, "show.html", context)
-
     fig = go.Figure()
     div = pio.to_html(fig, full_html=False)
     context = {
